9d7fa17bdfa6aa3e0c8f7e067ec93834f2a75daa
Peter Munro <peter@petermunro.org>
2018-01-16 19:03:20 +0000

    Initial import

---
+++ src/db.js
@@ -0,0 +1,24 @@
+let db = {
+  hubname: "untitled hub",
+  bootTime: new Date().toString(),
+  accessories: [
+    { id: 1, _type: "Light", name: "Hall Light", brightnessLevel: 100 },
+    {
+      id: 2,
+      _type: "Door",
+      name: "Front Door",
+      isClosed: true,
+      isLocked: true
+    },
+    {
+      id: 3,
+      _type: "Thermostat",
+      name: "Living Room Thermostat",
+      temperature: 72
+    },
+    { id: 4, _type: "Outlet", name: "Kitchen Outlet 1", isOn: true }
+  ],
+  rooms: []
+};
+
+module.exports = db;
---
+++ src/index.js
@@ -0,0 +1,69 @@
+let os = require("os");
+let express = require("express");
+let bodyParser = require("body-parser");
+let cors = require("cors");
+let { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+let { makeExecutableSchema } = require("graphql-tools");
+let { importSchema } = require("graphql-import");
+const db = require("./db.js");
+
+let moment = require("moment");
+let momentDurationFormatSetup = require("moment-duration-format");
+momentDurationFormatSetup(moment);
+
+let typeDefs = importSchema(`${__dirname}/schema.graphql`);
+
+let resolvers = {
+  Query: {
+    system: () => {
+      return {
+        hello: "worldsys",
+        hubname: db.hubname
+      };
+    },
+    accessories(_, { offset, limit }) {
+      console.log("offset:", offset, "limit:", limit);
+      return db.accessories;
+    }
+  },
+  Accessory: {
+    __resolveType(obj, context, info) {
+      return obj._type;
+    }
+  },
+  System: {
+    uptime() {
+      return moment.duration(os.uptime(), "seconds").format("h [hrs], m [min]");
+    },
+    hello(obj) {
+      console.log("obj:", obj);
+      return obj.hello + " from resolver";
+    },
+    uptimeDelayed() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(new Date().toString());
+        }, 3000);
+      });
+    }
+  },
+  Mutation: {
+    setHubname(_, { name }) {
+      console.log("Name:", name);
+      db.hubname = name;
+      return db.hubname;
+    }
+  }
+};
+
+let schema = makeExecutableSchema({ typeDefs, resolvers });
+
+let corsOptions = {
+  origin: "http://localhost:3000"
+};
+
+let app = express();
+app.use(cors(corsOptions));
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+app.listen(4001, () => console.log("Now browse to localhost:4000/graphiql"));
